03788cfc4e9324d0e5b643bc8b6481d26d4ded16
Xavi <xavier.tor@gmail.com>
2019-12-17 10:58:12 +0100

    Variable entorn mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:realtech@cluster0-ouqjn.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
